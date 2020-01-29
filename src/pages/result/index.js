76096b4dcec84738bf4687dc54a1a8a06dd7fe28
blackwidow <blackwidow@blackwidows-Mac-mini.local>
2020-01-29 11:02:44 +0700

    [CREATE] สร้าง Component Result

---
+++ src/pages/result/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Result(props) {
+  return (
+    <div id="result" className="table">
+      <h1>Search Result</h1>
+      <table>
+          <thead>
+              
+              <tr>
+                  <th>No</th>
+                  <th>Toy Name</th>
+                  <th>Price</th>
+                  <th>Brand</th>
+                  <th>View Detail</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+          </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Result;
