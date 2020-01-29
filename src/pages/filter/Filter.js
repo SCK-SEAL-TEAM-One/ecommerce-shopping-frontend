29a12db079f8b83ce6bad525788823ad8b1da76c
chill <chill@chills-Mac-mini.local>
2020-01-29 15:59:59 +0700

    [Edited] แก้ไขลำดับและคำผิดของ Filter

--- src/pages/filter/Filter.js
+++ src/pages/filter/Filter.js
@@ -6,34 +6,34 @@ import 'react-dropdown/style.css'
 function Filter(props) {
 
     const genderOptions = [
-        { value: 'M', label: 'Male' },
         { value: 'F', label: 'Female' },
-        { value: 'N', label: 'Neutral' },
+        { value: 'M', label: 'Male' },
+        { value: 'N', label: 'Neutral' }
     ]
 
     const ageOptions = [
-        { value: 1, label: 'Baby' },
-        { value: 2, label: 'Todler' },
         { value: 3, label: '3-5' },
         { value: 4, label: '6-8' },
-        { value: 5, label: 'Over 8' }
+        { value: 1, label: 'Baby' },
+        { value: 5, label: 'Over 8' },
+        { value: 2, label: 'Todler' }
     ]
 
     
 
     return (
         <div id='filter'>
-            <div id='ddl-gender'>
+            <div id='ddl_gender'>
                 <Dropdown options={genderOptions}
                     onChange={props.callOnChangeGender} value={props.gender}
                     placeholder="Select Gender" />
             </div>
-            <div id='ddl-age'>
+            <div id='ddl_age'>
                 <Dropdown options={ageOptions}
                     onChange={props.callOnChangeAge} value={props.age}
                     placeholder="Select Age" />
             </div>
-                <button onClick={e => props.history.push(`/result?age=${props.age.value}&gender=${props.gender.value}`)}>Search</button>
+                <button id='btn_filter' onClick={e => props.history.push(`/result?age=${props.age.value}&gender=${props.gender.value}`)}>Search</button>
         </div>
 
 
