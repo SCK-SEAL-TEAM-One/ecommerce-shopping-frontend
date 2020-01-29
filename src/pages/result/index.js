65b447e59ac295c7f19689240bb160ef48cc0f25
blackwidow <blackwidow@blackwidows-Mac-mini.local>
2020-01-29 16:46:09 +0700

    [Edited] แก้ไขหน้า Product list

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ResultRow from './row';
+import queryString from 'query-string'
 
-function Result(props) {
-  console.log(props)
-  return (
-    <div id="result" className="table">
-      <h1 id="txt_result">Search Result</h1>
-          {/* {props.match.params.gender}
-            {props.match.params.age} */}
-      <table>
-          <thead>
+class Result extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      products: [],
+    }
+    // this.fetchRow(props);
+  }
+
+  componentDidMount() {
+    this.fetchRow(this.props)
+ }
+  fetchRow(params) {
+    const values = queryString.parse(params.location.search)
+
+    // fetch(`http://localhost/api/v1/products?age_id=${encodeURIComponent(values.age)}&gerder_id=${encodeURIComponent(values.gender)}`, {
+    //   method: "GET", 
+    // })
+
+    this.setState({ products: [
+      { 
+        id: '30', 
+        name: 'Fisher-Price stroller', 
+        price: '25.99',
+        brand: 'CoolKidz' 
+      },
+      { 
+        id: '26', 
+        name: 'Les Dollie Toffe Apple', 
+        price: '24.95',
+        brand: 'CoolKidz' 
+      }
+    ]})
+
+  }
+
+  render() {
+    return (
+      <div id="result" className="table">
+        <h1 id="txt_result">Search Result</h1>
+        <table>
+            <thead>
+                
+                <tr>
+                    <th>No</th>
+                    <th>Toy Name</th>
+                    <th>Price</th>
+                    <th>Brand</th>
+                    <th>View Detail</th>
+                </tr>
+            </thead>
+            <tbody>
               
-              <tr>
-                  <th>No</th>
-                  <th>Toy Name</th>
-                  <th>Price</th>
-                  <th>Brand</th>
-                  <th>View Detail</th>
-              </tr>
-          </thead>
-          <tbody>
-            
-            
-            {/* {props.result.map((item, key) =>
-              <ResultRow item={item}></ResultRow>
-            )} */}
-          </tbody>
-      </table>
-    </div>
-  );
+              {this.state.products.map((product) => <ResultRow item={product} />)}
+
+            </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Result;
--- src/pages/result/row.js
+++ src/pages/result/row.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+  } from "react-router-dom";
 
 function ResultRow(props) {
-  return (
-      
+  return (  
     <tr>
-                  <td>{this.props.item.id}</td>
-                  <td>{this.props.item.name}</td>
-                  <td>{this.props.item.price}</td>
-                  <td>{this.props.item.brand}</td>
-                  <td></td>
-              </tr>
+        <td>{props.item.id}</td>
+        <td>{props.item.name}</td>
+        <td>{props.item.price}</td>
+        <td>{props.item.brand}</td>
+        <td>
+        <Link to={`/detail?id=${props.item.id}`}>
+        <button id={`btn_product_detail_${props.item.id}`}>View Details</button>
+        </Link>
+        </td>
+    </tr>
   );
 }
 
