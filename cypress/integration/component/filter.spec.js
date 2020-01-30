595764656d4a1d561d2b3a07ad6740a41656d45f
chill <chill@chills-Mac-mini.local>
2020-01-30 15:58:10 +0700

    [Edited] component test filter

--- cypress/integration/component/filter.spec.js
+++ cypress/integration/component/filter.spec.js
@@ -8,8 +8,9 @@ context('Actions', () => {
   it('filter product', () => {
     cy.get('#ddl_gender')
       .select('F')
-    
-
-      
+    cy.get('#ddl_age')
+      .select('3')
+    cy.get('#btn_filter').click()
+    cy.url().should('include', '/result?age=3&gender=F')
   })
 })
